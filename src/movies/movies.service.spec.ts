e0e3beee6070e8774daec2e0b354286802ca7c3f
Sunji <sophieadenn@gmail.com>
2021-08-01 02:19:40 +0900

    test cov 100%

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -45,4 +45,59 @@ describe('MoviesService', () => {
       }
     });
   });
+
+  describe('deleteOne', () => {
+    // delete success
+    it('delete a movie', () => {
+      service.create({
+        title: 'Test',
+        genres: ['test'],
+        year: 2000,
+      });
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+    it('should return a 404', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: 'Test',
+        genres: ['test'],
+        year: 2000,
+      });
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.create({
+        title: 'Test',
+        genres: ['test'],
+        year: 2000,
+      });
+      service.update(1, { title: 'Update Test' });
+      const moive = service.getOne(1);
+      expect(moive.title).toEqual('Update Test');
+    });
+    it('should throw a NotFoundException', () => {
+      try {
+        service.update(999, {});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
 });
