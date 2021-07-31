4d4efc5a397cd45487f987ad71ee46820bde93f2
Sunji <sophieadenn@gmail.com>
2021-08-01 01:14:07 +0900

    unit test 항목들 추가

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,4 +1,6 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import exp from 'constants';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
@@ -16,7 +18,31 @@ describe('MoviesService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should be 4', () => {
-    expect(2 + 2).toEqual(5);
+  describe('getAll()', () => {
+    // getAll 테스트
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array); // array 반환 테스트
+    });
+  });
+
+  describe('getOne', () => {
+    // getOne 테스트
+    it('should return a movie', () => {
+      service.create({
+        title: 'Test',
+        genres: ['test'],
+        year: 2000,
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+    });
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
